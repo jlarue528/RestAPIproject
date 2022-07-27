fd5aae764b9d045db6f930b09c336d5b2c9759eb
Jennifer LaRue <jennifer@photoshelter.com>
2022-07-27 12:44:18 -0400

    fixed spacing

--- auth-user.js
+++ auth-user.js
@@ -9,34 +9,33 @@ exports.authenticateUser = async (req, res, next) => {
     let message;
     try{
         const credentials = auth(req);
-        // console.log(credentials);
-
+        
         if(credentials) {
             const user = await Users.findOne({ where: {emailAddress: credentials.name} })
-        if(user) {
-            const authenticated = bcrypt
-                .compareSync(credentials.pass, user.confirmedPassword);
-            if(authenticated) {
-                console.log(`Authentication Successful for username: ${credentials.name}`)
+            if(user) {
+                const authenticated = bcrypt
+                    .compareSync(credentials.pass, user.confirmedPassword);
+                if(authenticated) {
+                    console.log(`Authentication Successful for username: ${credentials.name}`)
                 
-                req.currentUser = user;
+                    req.currentUser = user;
+                } else {
+                    message = `Authentication failure for username ${credentials.name}`;
+                }
             } else {
-                message = `Authentication failure for username ${credentials.name}`;
+                message = `User not found for username: ${credentials.name}`
             }
         } else {
-            message = `User not found for username: ${credentials.name}`
+            message = 'Auth header not found'
         }
-    } else {
-        message = 'Auth header not found'
-    }
 
-    if(message) {
-        console.warn(message);
-        res.status(401).json( { message: 'Access Denied'} )
-    } else {
-        next();
+        if(message) {
+            console.warn(message);
+            res.status(401).json( { message: 'Access Denied'} )
+        } else {
+            next();
+        }
+    } catch (err) {
+        res.json({error: err.message})
     }
-} catch (err) {
-    res.json({error: err.message})
-}
 }
